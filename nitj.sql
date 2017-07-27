196514d7489354dcf768e9c492dda175f9a86468
tanuj-ralli <30493619+tanuj-ralli@users.noreply.github.com>
2017-07-27 22:26:46 +0530

    Add files via upload

---
+++ nitj.sql
@@ -0,0 +1,74 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.4
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Jul 14, 2017 at 12:18 PM
+-- Server version: 5.6.12-log
+-- PHP Version: 5.4.16
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `nitj`
+--
+CREATE DATABASE IF NOT EXISTS `nitj` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `nitj`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `register`
+--
+
+CREATE TABLE IF NOT EXISTS `register` (
+  `email` varchar(200) NOT NULL,
+  `password` varchar(200) NOT NULL,
+  `name` varchar(200) NOT NULL,
+  `ques` varchar(200) NOT NULL,
+  `ans` varchar(200) NOT NULL,
+  PRIMARY KEY (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `register`
+--
+
+INSERT INTO `register` (`email`, `password`, `name`, `ques`, `ans`) VALUES
+('tanujralli97@gmail.com', '123369', 'Tanuj', 'Favourite Place', 'london');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `vote`
+--
+
+CREATE TABLE IF NOT EXISTS `vote` (
+  `email` varchar(200) NOT NULL,
+  `password` varchar(200) NOT NULL,
+  `name` varchar(200) NOT NULL,
+  `vote` varchar(200) NOT NULL,
+  `enteredname` varchar(200) NOT NULL,
+  `message` varchar(200) NOT NULL,
+  PRIMARY KEY (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `vote`
+--
+
+INSERT INTO `vote` (`email`, `password`, `name`, `vote`, `enteredname`, `message`) VALUES
+('ahn@gmail.com', '', '', '', 'ela', 'lololo'),
+('asd@gmail.com', '', '', '', 'qwerty', 'helooooooooo'),
+('lavya@gmail.com', '123123', 'Lavya Ralli', 'inc', 'abc', 'hii');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
